9c4298c2e06a77909a48c036372cba37d3b96865
rcavero <ramoncaveroaras@gmail.com>
2021-09-28 20:04:57 +0000

    modified to add some function and domains

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
+let dom = [".com",".es"];
+
 // Tenemos que crear todos los dominios posibles con la combinación de estos tres conjuntos de elementos
+
+const addDomain = (pron,ad,nou) => {
+    for(let a=0;a<dom.length;a++){
+        console.log(pron+ad+nou+dom[a]);
+    }
+}
 
 for(let i=0;i<pronoun.length;i++){
     for(let j=0;j<adj.length;j++){
         for(let k=0;k<noun.length;k++){
-            console.log(pronoun[i]+adj[j]+noun[k]+".com")
+            
+            //for(let w=0;w<dom.length;w++){
+                addDomain(pronoun[i],adj[j],noun[k])
+                //console.log(pronoun[i]+adj[j]+noun[k]+dom[w])
+            //}
         }
     }
 }
+// Now we are going to try to solve it by functions
+
