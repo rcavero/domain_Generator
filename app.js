6285e684ce0c296d50a9513176fd5535f8263d6c
rcavero <ramoncaveroaras@gmail.com>
2021-10-03 10:56:19 +0000

    added new features like changing petrus.us for petr.us

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
-let dom = [".com",".es"];
-
-// Tenemos que crear todos los dominios posibles con la combinación de estos tres conjuntos de elementos
+let noun = ['jogger','racoon','hornet','petrus'];
+let dom = [".com",".es",".net",".us"];
 
 const addDomain = (pron,ad,nou) => {
     for(let a=0;a<dom.length;a++){
-        console.log(pron+ad+nou+dom[a]);
+        if(nou.endsWith("com")&&dom[a]==".com"){
+            let nou2 = nou.replace("com","");
+            console.log(pron+ad+nou2+dom[a]);
+        }else if(nou.endsWith("es")&&dom[a]==".es"){
+            let nou2 = nou.replace("es","");
+            console.log(pron+ad+nou2+dom[a]);
+        }else if(nou.endsWith("net")&&dom[a]==".net"){
+            let nou2 = nou.replace("net","");
+            console.log(pron+ad+nou2+dom[a]);
+        }else if(nou.endsWith("us")&&dom[a]==".us"){
+            let nou2 = nou.replace("us","");
+            console.log(pron+ad+nou2+dom[a]);
+        }else{
+            console.log(pron+ad+nou+dom[a]);
+        }
     }
 }
 
 for(let i=0;i<pronoun.length;i++){
     for(let j=0;j<adj.length;j++){
         for(let k=0;k<noun.length;k++){
-            
-            //for(let w=0;w<dom.length;w++){
-                addDomain(pronoun[i],adj[j],noun[k])
-                //console.log(pronoun[i]+adj[j]+noun[k]+dom[w])
-            //}
+            addDomain(pronoun[i],adj[j],noun[k])
         }
     }
 }
-// Now we are going to try to solve it by functions
 
